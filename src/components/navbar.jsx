d98cc6d0542361893125515a059e3ffb3846bef5
yavnelf <54780131+Olga-Yavnel@users.noreply.github.com>
2020-10-08 00:22:24 +0200

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+// Stateless Functional Component
+const NavBar = (props) => {
+  console.log("NavBar - rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default NavBar;
